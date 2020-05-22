57e8f72ec99a369dc56a2c1d49b171e59ea5702f
Christopher William <v-chwilliam@egencia.com>
2020-05-23 01:31:04 +0530

    New Feauture implemented

--- File1.js
+++ File1.js
@@ -1,6 +1,7 @@
-First Line Hello World in Master
+Fiirst Line Hello World in Master
+Branch FIrst Line in between Master first and second Line
 Second Line Hello World in Master
-Third Line Hello World in Master
+Third Line [Changes from brnch] in Master
 
 
 After 2 Space above fourth line Hello World in Master
--- File2.js
+++ File2.js
@@ -4,6 +4,6 @@ SecondFile Hello World in Master third line
 
 SecondFile Hello World after one enter in Master fourth line
 SecondFile Hello World in Master fifth line
-
+Branch FIrst line below master fifth line
 
 SecondFile Hello World after 2 enters in Master sixth line
